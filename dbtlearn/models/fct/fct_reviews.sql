b1ef0c0ed9c9e800b7f74982cc0065098986996a
grey <kgeonhoe@gmail.com>
2024-08-17 02:20:46 +0900

    123

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -8,11 +8,18 @@ WITH src_reviews AS (
  SELECT * FROM {{ ref('src_reviews') }}
 )
 SELECT 
-  {{ dbt_utils.generat_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id
+  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id
    -- dbt_util 을 사용한 고유키 생성 (4컬럼을 병합한거와 같은 효과)
   ,*
  FROM src_reviews
 WHERE review_text is not null
 {% if is_incremental() %}
- AND review_date > (select max(review_date) from {{ this }})
+  {% if var("start_date", False) and var("end_date", False) %}
+    {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+    AND review_date >= '{{ var("start_date") }}'
+    AND review_date < '{{ var("end_date") }}'
+  {% else %}
+    AND review_date > (select max(review_date) from {{ this }})
+    {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True)}}
+  {% endif %}
 {% endif %}
